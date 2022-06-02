c38d757981056c481933e1cee2b1f1511c79ac2a
AndreSiepmann <85162125+AndreSiepmann@users.noreply.github.com>
2022-06-02 15:18:01 +0200

    Update coco.py

--- mmdet/datasets/coco.py
+++ mmdet/datasets/coco.py
@@ -595,7 +595,7 @@ class CocoDataset(CustomDataset):
                  logger=None,
                  jsonfile_prefix=None,
                  classwise=False,
-                 proposal_nums=(100, 300, 1000),
+                 proposal_nums=(3, 3, 3),
                  iou_thrs=None,
                  metric_items=None):
         """Evaluation in COCO protocol.
